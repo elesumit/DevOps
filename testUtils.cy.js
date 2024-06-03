bd55aa629b6975beec99e5486e1f0c6a79c3de4e
Sumit Gupta <62683761+elesumit@users.noreply.github.com>
2024-06-03 10:46:22 +0530

    Add files via upload

--- testUtils.cy.js
+++ testUtils.cy.js
@@ -7,84 +7,115 @@ const personalInsurancePage = require('../../pages/personalInsurance.js');
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
 });
+
 // Iterate over each viewport configuration
 Object.entries(config.viewPorts).forEach(([viewportName, viewportSetting]) => {
     // Run Cypress tests with the current viewport settings
     describe(`Validate the personal & business login for various account types - ${viewportName}`, () => {
-        beforeEach(() => {
-
-
-            cy.visit(config.environments.baseURL + "/" + config.environments.learningCenter);
-            if (viewportSetting.includes(':')) {
-                // Custom viewport size specified
-                const [width, height] = viewportSetting.split(',').map(value => parseInt(value.split(':')[1].trim()));
-                cy.viewport(width, height);
-            } else {
-                // Predefined device name
-                cy.viewport(viewportSetting);
-            }
-
-
-        });
-
-
-        it('Validate forgot business account type', () => {
+        it('Validate scenarios based on master CSV', () => {
             let currentPageUrl;
-            
-            // Store the current URL
-            cy.url().then(url => {
-                currentPageUrl = url;
-            });
-        
-            readCsvrows().then((csvData) => {
+
+            // Read the master CSV file
+            readCsvrows('cypress/fixtures/master.csv').then((csvData) => {
                 csvData.forEach(row => {
-                    const { Scenario, specname, execution, objType, childCsvPath } = row;
-                    //cy.log(`Execution: ${execution}, Link: ${csvtype}, url: ${childCsvPath}, expectetext: ${execution}`);
+                    const { Scenario, baseURL, URLextension, specname, execution, objType, childCsvPath } = row;
+
                     if (execution === 'yes') {
+                        // Get the base URL and URL extension from the config file using the keys from the master CSV
+                        const environmentURL = config.environments[baseURL];
+                        const extensionURL = config.environments[URLextension];
+
+                        // Log the environmentURL, extensionURL to debug
+                        cy.log(`Environment URL: ${environmentURL}`);
+                        cy.log(`Extension URL: ${extensionURL}`);
+                    
+
+                        const fullURL = `${environmentURL}/${extensionURL}`;
+                        cy.log(`Visiting URL: ${fullURL}`);
+
+                        // Visit the dynamically constructed URL
+                        cy.visit(fullURL);
+
+                        // Set viewport if specified
+                        if (viewportSetting.includes(':')) {
+                            // Custom viewport size specified
+                            const [width, height] = viewportSetting.split(',').map(value => parseInt(value.split(':')[1].trim()));
+                            cy.viewport(width, height);
+                        } else {
+                            // Predefined device name
+                            cy.viewport(viewportSetting);
+                        }
+
+                        cy.url().then(url => {
+                            currentPageUrl = url;
+                        });
+
                         readCsv(`cypress/fixtures/${childCsvPath}`).then((childRows) => {
+                            const zip = pageControllers.getZipCode(childRow['pageName'], childRow['zipElement']);
+                            const quote = pageControllers.getQuote(childRow['getQuoteButtonReference'], childRow['quoteButton']);
+                            const xpath = pageControllers.getXPath(childRow['condition'], childRow['linkText']);
+                            cy.log(childRow);
                             childRows.forEach((childRow) => {
                                 if (objType.includes('quote')) {
                                     cy.log(childRow['productType'], childRow['zipCode']);
 
-                                    pageControllers.selectDropdownOption("selectQuoteForm",childRow['insuranceType']);
-                                    personalInsurancePage.zipCode(childRow['productType'], childRow['zipCode']);
-                                    personalInsurancePage.clickOnQuote(childRow['prop_startquote']);
-                                    pageControllers.validatePageText(childRow['expectedText']);
-                                    cy.go('back');
+                                    
 
+                                    pageControllers.selectDropdownOption("selectQuoteForm", childRow['insuranceType']);
+
+
+                                    if (childRow['insuranceType']) {
+                                        pageControllers.selectDropdownOption("selectQuoteForm", childRow['insuranceType']);
+
+                                        
+                                    }
+                                    else{
+                                        pageControllers.clickLink
+
+                                    }
+
+                                    if (childRow['zipCode'] !== '' && childRow['zipCode'] !== undefined) {
+                                        pageControllers.enterZip(zip,childRow['zipCode'])
+                                        
+                                        pageControllers.clickQuote(quote);
+                                       
+                                        pageControllers.validatePageText(childRow['expectedText']);
+                                        // cy.go('back');
+                                        cy.visit(currentPageUrl);
+                                    }
+                                    else {
+                                       
+                                       pageControllers.clickQuote(quote);
+                                        pageControllers.validatePageText(childRow['expectedText']);
+                                        //cy.go('back');
+                                        cy.visit(currentPageUrl);
+                                    }
 
                                 } else if (objType.includes('link')) {
                                     cy.log(childRow['expectedText'], childRow['condition']);
 
-
                                     // Get the XPath for the given condition and link
-                                    const xpath = pageControllers.getXPath(childRow['condition'], childRow['linkText']);
+                                   
 
                                     // Click the link using the generic click function
                                     if (xpath) {
                                         pageControllers.clickLink(xpath);
                                         cy.wait(2000);
                                         cy.contains(childRow['expectedText']).should('be.visible');
-                                       cy.visit(currentPageUrl);
+                                        cy.visit(currentPageUrl);
                                     } else {
-                                        cy.log(`No matching XPath for condition: ${condition}`);
+                                        cy.log(`No matching XPath for condition: ${childRow['condition']}`);
                                     }
-
                                 }
-
                             });
+                        }).catch((error) => {
+                            console.error(`Error reading child CSV: ${error}`);
                         });
                     }
-
                 });
+            }).catch((error) => {
+                console.error(`Error reading master CSV: ${error}`);
             });
         });
-
-
-
-
-
     });
-
 });
-
